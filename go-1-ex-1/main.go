4dd0b0e79d892aa2f1cc32bbe0ffe2fea2b11782
Ylldrit <gradualisht@gmail.com>
2025-11-03 15:18:19 +0100

    Go 1 – Variablen + formatierte Ausgabe

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -3,10 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	// TODO: Declare and initialize the variables being used in the output!
-	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
-	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
-	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
-	fmt.Printf("Grösse (in Metern): %.2f\n", heightInMeters)
-	fmt.Printf("Sternzeichen: %c\n", zodiacSign)
+    var firstName string = "Ylldrit"
+    var lastName string = "Abazi"
+    var dayOfBirth int = 17
+    var monthOfBirth string = "August"
+    var yearOfBirth int = 2008
+    var numberOfSiblings int = 2
+    var heightInMeters float64 = 1.80
+    var zodiacSign string = "Löwe"
+
+    fmt.Printf("Name: %s %s\n", firstName, lastName)
+    fmt.Printf("Geburtsdatum: %d. %s %d\n", dayOfBirth, monthOfBirth, yearOfBirth)
+    fmt.Printf("Geschwister: %d\n", numberOfSiblings)
+    fmt.Printf("Grösse: %.2f m\n", heightInMeters)
+    fmt.Printf("Sternzeichen: %s\n", zodiacSign)
 }
