af320d4c1d0c2e12905815ab5f87476445e2b158
Ylldrit <gradualisht@gmail.com>
2025-11-24 14:37:54 +0100

    go-3-ex-4: spielkarten mit verschachtelten for-range schleifen

--- go-3-ex-4/main.go
+++ go-3-ex-4/main.go
@@ -3,28 +3,29 @@ package main
 import "fmt"
 
 const (
-	Diamonds = '\u25c6' // Karo
-	Spades   = '\u2660' // Pik
-	Clubs    = '\u2663' // Kreuz
-	Hearts   = '\u2665' // Herz
-
-	Six   = '\u2465'
-	Seven = '\u2466'
-	Eight = '\u2467'
-	Nine  = '\u2468'
-	Ten   = '\u2469'
-	Jack  = 'J'
-	Queen = 'Q'
-	King  = 'K'
-	Ace   = 'A'
+	Diamonds = '\u25c6' // ◆
+	Spades   = '\u2660' // ♠
+	Clubs    = '\u2663' // ♣
+	Hearts   = '\u2665' // ♥
+	Six      = '\u2465'
+	Seven    = '\u2466'
+	Eight    = '\u2467'
+	Nine     = '\u2468'
+	Ten      = '\u2469'
+	Jack     = 'J'
+	Queen    = 'Q'
+	King     = 'K'
+	Ace      = 'A'
 )
 
 func main() {
 	suits := []rune{Diamonds, Spades, Clubs, Hearts}
 	ranks := []rune{Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace}
 
-	// TODO: Loop over suits and ranks to output all combinations.
-
-	// TODO: delete this line afterwards
-	fmt.Println(suits, ranks)
+	for _, rank := range ranks {
+		for _, suit := range suits {
+			fmt.Printf("%c%c\t", suit, rank)
+		}
+		fmt.Println()
+	}
 }
