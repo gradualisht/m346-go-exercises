9d2e301b901cec0c4d5099158e873d41de1d6139
gradualisht <gradualisht@gmail.com>
2025-12-01 14:32:57 +0100

    go-4-ex-4: temperaturumrechnungen + methoden für celsius/fahrenheit

--- go-4-ex-4/main.go
+++ go-4-ex-4/main.go
@@ -1,9 +1,42 @@
 package main
 
-// TODO: implement the function convertCelsiusToFahrenheit
-// TODO: implement the function convertFahrenheitToCelsius
+import "fmt"
+
+// typdefinitionen
+type Celsius float64
+type Fahrenheit float64
+
+// funktionen für umrechnung
+func convertCelsiusToFahrenheit(c float64) float64 {
+	return c*9/5 + 32
+}
+func convertFahrenheitToCelsius(f float64) float64 {
+	return (f - 32) * 5 / 9
+}
+
+// methoden auf typen
+func (c Celsius) ConvertToFahrenheit() Fahrenheit {
+	return Fahrenheit(c*9/5 + 32)
+}
+func (f Fahrenheit) ConvertToCelsius() Celsius {
+	return Celsius((f - 32) * 5 / 9)
+}
 
 func main() {
-	// TODO: call the function convertCelsiusToFahrenheit
-	// TODO: call the function convertFahrenheitToCelsius
+	// funktionsaufrufe
+	fmt.Println(convertCelsiusToFahrenheit(0))   
+	fmt.Println(convertCelsiusToFahrenheit(23))  
+	fmt.Println(convertCelsiusToFahrenheit(100)) 
+
+	// rückrechnung
+	fmt.Println(convertFahrenheitToCelsius(32))  
+	fmt.Println(convertFahrenheitToCelsius(73.4))
+	fmt.Println(convertFahrenheitToCelsius(212)) 
+
+	// methodenaufrufe
+	var cozy Celsius = 23
+	fmt.Println(cozy.ConvertToFahrenheit())
+
+	var cold Fahrenheit = 15.3
+	fmt.Println(cold.ConvertToCelsius()) 
 }
