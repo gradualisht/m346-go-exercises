f4fa192126cb27d17a29c8d7ca4675935082fb56
gradualisht <gradualisht@gmail.com>
2025-12-01 14:28:21 +0100

    go-4-ex-2: hypotenuse funktion + methode shortSides

--- go-4-ex-2/main.go
+++ go-4-ex-2/main.go
@@ -1,7 +1,35 @@
 package main
 
-// TODO: implement the function computeHypotenuse using math.Pow and math.Sqrt
+import (
+	"fmt"
+	"math"
+)
+
+// computeHypotenuse berechnet Hypotenuse c via formel
+func computeHypotenuse(a, b float64) float64 {
+	return math.Sqrt(math.Pow(a, 2) + math.Pow(b, 2))
+}
+
+// truct-Typ 
+type ShortSides struct {
+	A float64
+	B float64
+}
+
+// hypotenuse ist methode 
+func (s ShortSides) Hypotenuse() float64 {
+	return math.Sqrt(math.Pow(s.A, 2) + math.Pow(s.B, 2))
+}
 
 func main() {
-	// TODO: call the function computeHypotenuse
+	// funktionstests
+	fmt.Println(computeHypotenuse(3, 4)) // 5
+	fmt.Println(computeHypotenuse(5, 12)) // 13
+	fmt.Println(computeHypotenuse(8, 15)) // 17
+
+	// methoden-tests
+	pairs := []ShortSides{{3, 4}, {5, 12}, {8, 15}}
+	for _, p := range pairs {
+		fmt.Printf("(%.0f, %.0f) → %.1f\n", p.A, p.B, p.Hypotenuse())
+	}
 }
